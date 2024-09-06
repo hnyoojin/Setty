fa630c918cfc52353ac9ca12d948e17d9dead699
hnyoojin <0dbwls21@gmail.com>
2024-09-07 01:25:38 +0900

    test commit 2

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
+import * as React from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+
+import HomeScreen from "./screens/HomeScreen";
+import CalendarScreen from "./screens/CalendarScreen";
+import QuestScreen from "./screens/QuestScreen";
+import MessageScreen from "./screens/MessageScreen";
+
+const Tab = createBottomTabNavigator();
+const { width:SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.navBar}>
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="Home">
+          <Tab.Screen name="Home" component={HomeScreen}/>
+          <Tab.Screen name="Calendar" component={CalendarScreen}/>
+          <Tab.Screen name="Message" component={MessageScreen}/>
+          <Tab.Screen name="Quest" component={QuestScreen}/>
+        </Tab.Navigator>
+      </NavigationContainer>
+
+      <StatusBar style="dark"/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  navBar: {
+    width: SCREEN_WIDTH,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
   },
-});
+})
